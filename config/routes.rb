efb254b548d676c4ffde1b585b74e233c6bbbadd
bart dangus <brskythomas@gmail.com>
2015-08-05 20:03:37 -0700

    css

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/about'
   get 'welcome/contact'
   root to: 'welcome#index' #root is just a hash that takes as an argument
-end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
